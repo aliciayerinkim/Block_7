c98b8b7127d360b00289e265c81132cd41f9906c
aliciayerinkim <aliciayerinkim@gmail.com>
2024-07-22 10:34:19 -0700

    javascript done

---
+++ script.js
@@ -0,0 +1,15 @@
+// Initialize variables for each part of the vault combination
+// Subtraction to get 10
+let codeOne = 50 - 40; 
+
+// Multiplication to get 40
+let codeTwo = 10 * 4; 
+
+// Addition to get 39
+let codeThree = 36 + 3;
+
+// Combine the results into a single string
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: " + codeOne + " - " + codeTwo + " - " + codeThree;
+
+// Show the vault codes in a pop-up using alert
+alert(message);
